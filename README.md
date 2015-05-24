52eefec05ae2acb2090f546ae46e69dba304cc18
Tomasz Janiszewski <janiszt@gmail.com>
2015-05-24 14:41:04 +0200

    Generify script

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A connect4 playing solution based on a Kohonnen network.
 
 ## Usage
 
-`octave script/kohonnect4.m`
+`octave --quiet script/kohonnect4.m  data/board_2x2.csv `
--- script/kohonnect4.m
+++ script/kohonnect4.m
@@ -6,42 +6,35 @@ function normalized = normalize(x)
   normalized(!isfinite(normalized))=0;
 end
 
-weights = normalize(exprnd(1,2*4,1))';
-examples = [
-normalize(
-  [ 0, 0, \
-    0, 0 ]);
-normalize(
-  [ 0, 0, \
-    1,-1 ]);
-normalize(
-  [ 0, 0, \
-    0,-1 ]);
-normalize(
-  [ 0, 0, \
-    0, 1 ]);
-];
-answers = [
-  1;
-  1;
-  2;
-  2;
-];
+WIDTH = 2;
+HEIGHT = 2;
+INPUTS = WIDTH*HEIGHT;
+
+weights = normalize(exprnd(1,WIDTH*INPUTS,1))';
+
+data = csvread(argv(){1});
+examples = data(:, 1:INPUTS);
+answers = data(:, INPUTS+1);
 
 for iteration = 1:1000
   for example_index = 1:size(examples)
     example = examples(example_index, :);
-    from = 1+(answers(example_index)-1)*4;
-    to = answers(example_index)*4;
+    from = 1+(answers(example_index))*INPUTS;
+    to = (answers(example_index)+1)*INPUTS;
     winner_weights = weights(from:to);
     weights(from:to) = weights(from:to) + (example-winner_weights);
-    weights = normalize(weights);
+    weights = normalize(weights); %Should we normalize all weights or only updated ones
+    %weights(from:to) = normalize(weights(from:to))
   endfor
 endfor
 
 for example_index = 1:size(examples)
   input_signal = weights.*[examples(example_index,:),examples(example_index,:)];
-  neurons_outputs = [sum(input_signal(1:4)), sum(input_signal(5:8))];
+  output_index = 1:INPUTS: length(weights);
+  neurons_outputs = zeros(WIDTH, 1);
+  for output = 2:WIDTH
+    neurons_outputs(output-1) = [sum(input_signal(output_index(output-1):output_index(output)))];
+  endfor
   [value, index] =  max(neurons_outputs);
-  printf("Expected: %d%% Actual: %d%%\n", answers(example_index), index);
+  printf("Expected: %d%% Actual: %d%%\n", answers(example_index), index-1);
 endfor
