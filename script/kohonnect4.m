e1d8965259a22f265ea82443675c05cfb3b0a8e6
Tomasz Janiszewski <janiszt@gmail.com>
2015-06-04 19:37:05 +0200

    Compute score on training and testing set

--- script/kohonnect4.m
+++ script/kohonnect4.m
@@ -6,11 +6,31 @@ function normalized = normalize(x)
   normalized(!isfinite(normalized))=0;
 end
 
-WIDTH = 7;
-HEIGHT = 6;
-INPUTS = WIDTH*HEIGHT;
-RATE = 0.001;
-ITERATIONS = 100;
+global WIDTH = 7;
+global HEIGHT = 6;
+global INPUTS = WIDTH*HEIGHT;
+global RATE = 0.00001;
+global ITERATIONS = 1000;
+
+function score = compute_score(weights, test_examples, test_answers)
+  global WIDTH
+  global INPUTS
+  actuals = test_answers;
+  for example_index = 1:size(test_examples)
+    input = repmat(test_examples(example_index,:), 1, WIDTH);
+    input_signal = weights.*input;
+    output_index = 1:INPUTS: length(weights);
+    neurons_outputs = zeros(WIDTH, 1);
+    for output = 2:WIDTH
+      neurons_outputs(output-1) = [sum(input_signal(output_index(output-1):output_index(output)))];
+    endfor
+    [value, index] =  max(neurons_outputs);
+    actuals(example_index) = index-1;
+  endfor
+  score = sum((actuals-test_answers) == 0) / length(test_answers);
+end
+
+
 
 weights = rand(1,WIDTH*INPUTS);
 
@@ -26,35 +46,19 @@ test_answers = test_data(:, INPUTS+1);
 examples = data(:, 1:INPUTS);
 answers = data(:, INPUTS+1);
 
+printf("iteration,training_score,test_score\n")
+
 for iteration = 1:ITERATIONS
+  RATE = 1 / (1+iteration);
   for example_index = 1:size(examples)
     example = examples(example_index, :);
     from = 1+(answers(example_index))*INPUTS;
     to = (answers(example_index)+1)*INPUTS;
     weights(from:to) = normalize(weights(from:to) + RATE*(example-weights(from:to)));
   endfor
-  %RATE = 1/INPUTS;
-  %RATE = RATE / 2;
-  %RATE = RATE / e;
-  RATE = RATE * exp(-iteration*HEIGHT/(200*ITERATIONS));
-  %RATE = RATE / 10;
-  %RATE = 1/iteration;
-  %RATE = 1/iteration^2;
-  %RATE = 1/log(iteration)^2;
+  test_score = compute_score(weights, test_examples, test_answers);
+  training_score = compute_score(weights, examples, answers);
+  printf("%d,%f,%f\n", iteration , training_score, test_score)
 
-  actuals = test_answers;
-  for example_index = 1:size(test_examples)
-    input = repmat(test_examples(example_index,:), 1, WIDTH);
-    input_signal = weights.*input;
-    output_index = 1:INPUTS: length(weights);
-    neurons_outputs = zeros(WIDTH, 1);
-    for output = 2:WIDTH
-      neurons_outputs(output-1) = [sum(input_signal(output_index(output-1):output_index(output)))];
-    endfor
-    [value, index] =  max(neurons_outputs);
-    actuals(example_index) = index-1;
-  endfor
-  score = sum((actuals-test_answers) == 0) / length(test_answers);
-  printf("%d,%f\n", iteration , score)
 
 endfor
